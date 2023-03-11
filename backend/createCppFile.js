f0977c7407d6b5655eaeef6ea9499e9f67f30d5d
Ankan Hazra <ankanhazraeducation@gmail.com>
2023-03-11 15:32:19 +0530

    add create file and modify server file

---
+++ backend/createCppFile.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+const { v4: uuid } = require("uuid");
+
+const codesDir = path.join(__dirname, "codes");
+
+if (!fs.existsSync(codesDir)) {
+  fs.mkdirSync(codesDir, { recursive: true });
+};
+
+const createCppFile = async (language, code) => {
+  const jobId = uuid();
+  const fileName = `${jobId}.${language}`;
+  const filePath = path.join(codesDir, fileName);
+  await fs.writeFileSync(filePath, code);
+  return filePath;
+};
+
+module.exports = {
+  createCppFile
+};
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
-const express = require('express')
+const express = require('express');
+const { createCppFile } = require('./createCppFile');
 
-const app = express()
-app.use(express.urlencoded({extended: true}))
+const app = express();
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get('/', (req, res)=>{
-    return res.json({Hello:"world"})
+    return res.json({Hello:"world"});
 })
 
-app.post('/run', (req, res)=>{
+app.post('/run', async (req, res)=>{
 
-    const {language = "cpp", code} = req.body
+    const {language = "cpp", code} = req.body;
 
     if(code === undefined){
-        return res.status(400).json({success:false, error:"empty code"})
+        return res.status(400).json({success:false, error:"empty code"});
     }
-    return res.json({language, code})
+
+    const filePath = await createCppFile(language, code);
+
+    return res.json({filePath});
 })
 
 app.listen(5000, ()=>{
-    console.log("Listening on port 5000")
+    console.log("listening on port 5000");
 })
