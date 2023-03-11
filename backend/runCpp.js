994b54766f2899f43c28b8e35726814bd9c21bd5
Ankan Hazra <ankanhazraeducation@gmail.com>
2023-03-11 17:22:07 +0530

    add runCpp file

---
+++ backend/runCpp.js
@@ -0,0 +1,30 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+const { resolve } = require('path');
+const path = require('path');
+
+const outputsDir = path.join(__dirname, "outputs");
+
+if (!fs.existsSync(outputsDir)) {
+  fs.mkdirSync(outputsDir, { recursive: true });
+};
+
+const runCpp = (filePath) =>{
+    const jobId = path.basename(filePath).split(".")[0];
+    const outputPath = path.join(outputsDir, `${jobId}.out`);
+
+    return new Promise((resolve, reject) => {
+    exec(
+      `g++ ${filePath} -o ${outputPath} && cd ${outputsDir} && ./${jobId}.out`,
+      (error, stdout, stderr) => {
+        error && reject({ error, stderr });
+        stderr && reject(stderr);
+        resolve(stdout);
+      }
+    );
+  });
+}
+
+module.exports= {
+    runCpp
+};
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,32 @@
 const express = require('express');
+
 const { createCppFile } = require('./createCppFile');
+const { runCpp } = require('./runCpp');
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+app.get("/", (req, res)=>{
     return res.json({Hello:"world"});
 })
 
-app.post('/run', async (req, res)=>{
+app.post("/run", async (req, res)=>{
 
     const {language = "cpp", code} = req.body;
 
     if(code === undefined){
         return res.status(400).json({success:false, error:"empty code"});
     }
-
+    try{
     const filePath = await createCppFile(language, code);
+        
+    const output = await runCpp(filePath);
 
-    return res.json({filePath});
+    return res.json({filePath, output});
+    } catch(error){
+        return res.status(500).json({error});
+    }
 })
 
 app.listen(5000, ()=>{
